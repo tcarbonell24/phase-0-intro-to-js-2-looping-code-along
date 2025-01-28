65a49a468f4b43fc4868be7d0f607a58db0a420f
Thomas Carbonell <tcarbonell12@gmail.com>
2025-01-28 00:02:08 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const occasion =  "birthday";
+
+function writeCards(names, occasion) {
+  let cards = [];
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    console.log(cards);
+  }
+  return cards;
+}
+
+writeCards(names, occasion);
+
+function countDown() {
+  let i = 10;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
+
+countDown();
